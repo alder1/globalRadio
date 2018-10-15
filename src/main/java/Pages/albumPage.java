a52902d11f12710fa9faa9d4a352548ba95c24a2
alder1 <alder1.amu@gmail.com>
2018-10-15 23:40:32 +0100

    update on albumPage

--- src/main/java/Pages/albumPage.java
+++ src/main/java/Pages/albumPage.java
@@ -1,9 +1,17 @@
 package Pages;
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.awt.*;
+
+import static Pages.pageClick.legacyInteractWithFileDialogue;
 
 public class albumPage {
     WebDriver driver;
@@ -16,8 +24,26 @@ public class albumPage {
     @FindBy(xpath = ".//*[@id='ember205']/section[1]/div/div[2]/div[1]/div[2]/header/h1/span[1]")
     private WebElement promisesAlbum;
 
-     public boolean assertpromisesAlbum(){
+
+
+     public boolean assertpromisesAlbum() throws Exception {
+         JavascriptExecutor jsx = (JavascriptExecutor)driver;
+         jsx.executeScript("window.confirm('Alert dismiss in 5 sec');");
+
+         try {
+             Alert confirmation = driver.switchTo().alert();
+             String alertText =confirmation.getText();
+             System.out.println("Alert text is :" +alertText);
+             Thread.sleep(5000);
+             confirmation.dismiss();
+         }catch (Exception e){
+             System.out.println(e);
+         }
+
+
          return promisesAlbum.isDisplayed();
+
+
      }
 
 
